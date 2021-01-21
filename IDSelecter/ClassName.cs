42dd4eadc3f6c10bac0f528a4e51e6d2aeb2af89
Thisal Kodithuwakku <33801630+Harryjer@users.noreply.github.com>
2021-01-21 21:51:24 +0530

    Add project files.

---
+++ IDSelecter/ClassName.cs
@@ -0,0 +1,11 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+{
+    class ClassName
+    {
+        IWebDriver driver = new ChromeDriver();
+
+    
+    }
+}
---
+++ IDSelecter/IdSelecter.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+class IdSelecter
+{
+    static void Main()
+    {
+        string url = "https://testing.todorvachev.com/selectors/id/";
+        string id = "testImage";
+
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(url);
+
+        IWebElement element = driver.FindElement(By.Id(id));
+
+        if(element.Displayed)
+        {
+            GreenMessage("Element is displayed");
+        }
+        else
+                {
+            RedMessage("Element is not displayed");
+        }
+        driver.Quit();
+
+
+    }
+
+    private static void RedMessage(string message)
+    {
+        System.Console.ForegroundColor = System.ConsoleColor.Red;
+        System.Console.WriteLine(message);
+        System.Console.ForegroundColor = System.ConsoleColor.White;
+    }
+
+    private static void GreenMessage(string message)
+    {
+        System.Console.ForegroundColor = System.ConsoleColor.Green;
+        System.Console.WriteLine(message);
+        System.Console.ForegroundColor = System.ConsoleColor.White;
+    }
+}
+
